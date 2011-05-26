01242c2e60fcb72a04797f0c2c4e3eb8a87db5b8
Sethcran <sethcran@gmail.com>
2011-05-26 17:54:29 -0400

    Introduced database connection

--- src/com/sethcran/cityscape/Cityscape.java
+++ src/com/sethcran/cityscape/Cityscape.java
@@ -1,17 +1,21 @@
 package com.sethcran.cityscape;
 
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.logging.Logger;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.config.Configuration;
 
-import com.mysql.jdbc.Connection;
 
 public class Cityscape extends JavaPlugin {
-	static public Logger log = null;
-	CSSettings settings;
-	Connection con = null;
+	public static Logger log = null;
+	private CSSettings settings = null;
+	private Connection con = null;
 
 	@Override
 	public void onDisable() {
@@ -24,6 +28,7 @@ public class Cityscape extends JavaPlugin {
 		log.info("Cityscape loaded.");
 		
 		settings = new CSSettings();
+		loadDBConnection();
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, 
@@ -31,6 +36,26 @@ public class Cityscape extends JavaPlugin {
 		if(cmd.getName().equalsIgnoreCase("test"))
 			sender.sendMessage("works.");
 		return true;
+	}
+	
+	private void loadDBConnection() {
+		File file = new File("bukkit.yml");
+		Configuration config = new Configuration(file);
+		config.load();
+		
+		String username = config.getString("database.username");
+		String password = config.getString("database.password");
+		String driver = config.getString("database.driver");
+		String url = config.getString("database.url");
+		
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, username, password);
+		} catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
