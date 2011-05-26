af3dd992a6454335818bd9cdce2236b5707d6daa
Sethcran <sethcran@gmail.com>
2011-05-26 15:48:59 -0400

    Initial settings class

---
+++ src/com/sethcran/cityscape/CSSettings.java
@@ -0,0 +1,37 @@
+package com.sethcran.cityscape;
+
+import java.io.File;
+
+import org.bukkit.util.config.Configuration;
+
+
+public class CSSettings {
+	public boolean debug;
+	
+	CSSettings() {
+		String directory = "plugins/Cityscape";
+		File dataFolder = new File(directory);
+		dataFolder.mkdir();
+		File file = new File(directory + File.separator + "config.yml");
+		Configuration config = new Configuration(file);
+		config.load();
+		
+		if(!file.exists())
+			create(file);
+		else
+			debug = config.getBoolean("debug", false);
+	}
+	
+	public void create(File file) {
+		try {
+			file.createNewFile();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		Configuration config = new Configuration(file);
+		config.load();
+		
+		config.setProperty("debug",	false);
+		config.save();
+	}
+}
--- src/com/sethcran/cityscape/Cityscape.java
+++ src/com/sethcran/cityscape/Cityscape.java
@@ -6,23 +6,29 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.mysql.jdbc.Connection;
+
 public class Cityscape extends JavaPlugin {
+	static public Logger log = null;
+	CSSettings settings;
+	Connection con = null;
 
 	@Override
 	public void onDisable() {
-		Logger log = Logger.getLogger("Minecraft");
 		log.info("Cityscape unloaded.");
 	}
 
 	@Override
 	public void onEnable() {
-		Logger log = Logger.getLogger("Minecraft");
+		log = Logger.getLogger("Minecraft");
 		log.info("Cityscape loaded.");
+		
+		settings = new CSSettings();
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, 
 			String commandLabel, String[] args) {
-		if( cmd.getName().equalsIgnoreCase("test"))
+		if(cmd.getName().equalsIgnoreCase("test"))
 			sender.sendMessage("works.");
 		return true;
 	}
