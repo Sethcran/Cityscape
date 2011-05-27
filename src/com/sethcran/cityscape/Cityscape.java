4c6b0e747efd5ade0ce47b4c9a4fb4e29990bc5b
Sethcran <sethcran@gmail.com>
2011-05-27 01:31:04 -0400

    Initial permissions support.

--- src/com/sethcran/cityscape/Cityscape.java
+++ src/com/sethcran/cityscape/Cityscape.java
@@ -4,13 +4,17 @@ import java.util.logging.Logger;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.nijiko.permissions.PermissionHandler;
+import com.nijikokun.bukkit.Permissions.Permissions;
 import com.sethcran.cityscape.database.Database;
 
 
 public class Cityscape extends JavaPlugin {
 	public static Logger log = null;
+	public static PermissionHandler permissionHandler = null;
 	
 	public static Settings settings = null;
 	public static Database database = null;
@@ -23,10 +27,12 @@ public class Cityscape extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		log = Logger.getLogger("Minecraft");
-		log.info("Cityscape loaded.");
+		setupPermissions();
 		
 		settings = new Settings();
 		database = new Database();
+		
+		log.info("Cityscape loaded.");
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, 
@@ -34,5 +40,17 @@ public class Cityscape extends JavaPlugin {
 		if(cmd.getName().equalsIgnoreCase("test"))
 			sender.sendMessage("works.");
 		return true;
+	}
+	
+	private void setupPermissions() {
+		Plugin permissionsPlugin = this.getServer().getPluginManager().getPlugin("Permissions");
+		
+		if(permissionHandler == null) {
+			if(permissionsPlugin != null) {
+				permissionHandler = ((Permissions)permissionsPlugin).getHandler();
+			} else {
+				log.info("Permission system not detected, defaulting to OP");
+			}
+		}
 	}
 }
