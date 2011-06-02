f9c493b1ab7fa5acbc0cf6b0836340ab185aacd4
Sethcran <sethcran@gmail.com>
2011-06-01 21:40:16 -0400

    Added the ability to decline invitations and to list invitiations.

--- src/com/sethcran/cityscape/commands/citycommands/Decline.java
+++ src/com/sethcran/cityscape/commands/citycommands/Decline.java
@@ -1,8 +1,10 @@
 package com.sethcran.cityscape.commands.citycommands;
 
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import com.sethcran.cityscape.Cityscape;
+import com.sethcran.cityscape.Constants;
 import com.sethcran.cityscape.commands.CSCommand;
 
 public class Decline extends CSCommand {
@@ -16,7 +18,41 @@ public class Decline extends CSCommand {
 
 	@Override
 	public void execute(CommandSender sender, String[] args) {
-
+		Player player = null;
+		if(sender instanceof Player)
+			player = (Player)sender;
+		else {
+			sender.sendMessage(Constants.CITYSCAPE + Constants.ERROR_COLOR +
+					"Only players in game can use this command.");
+			return;
+		}
+		
+		if(args == null) {
+			player.sendMessage(Constants.CITYSCAPE + Constants.ERROR_COLOR +
+					"You must provide a city name to decline.");
+			return;
+		}
+		
+		if(args.length > 1) {
+			player.sendMessage(Constants.CITYSCAPE + Constants.ERROR_COLOR +
+					"Please provide only a single city name to decline.");
+			return;
+		}
+		
+		if(!plugin.getDB().doesCityExist(args[0])) {
+			player.sendMessage(Constants.CITYSCAPE + Constants.ERROR_COLOR +
+					"That city does not exist.");
+			return;
+		}
+		
+		if(!plugin.getDB().isInvited(player.getName(), args[0])) {
+			player.sendMessage(Constants.CITYSCAPE + Constants.ERROR_COLOR +
+					"You have not been invited to join " + args[0] + ".");
+			return;
+		}
+		
+		plugin.getDB().removeInvite(player.getName(), args[0]);
+		player.sendMessage(Constants.CITYSCAPE + Constants.ERROR_COLOR +
+				"You have declined the invitation to join the city of " + args[0] + ".");
 	}
-
 }
--- src/com/sethcran/cityscape/commands/citycommands/Invites.java
+++ src/com/sethcran/cityscape/commands/citycommands/Invites.java
@@ -1,8 +1,12 @@
 package com.sethcran.cityscape.commands.citycommands;
 
+import java.util.ArrayList;
+
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import com.sethcran.cityscape.Cityscape;
+import com.sethcran.cityscape.Constants;
 import com.sethcran.cityscape.commands.CSCommand;
 
 public class Invites extends CSCommand {
@@ -16,7 +20,37 @@ public class Invites extends CSCommand {
 
 	@Override
 	public void execute(CommandSender sender, String[] args) {
-
+		Player player = null;
+		if(sender instanceof Player)
+			player = (Player)sender;
+		else {
+			sender.sendMessage(Constants.CITYSCAPE + Constants.ERROR_COLOR +
+					"Only players in game can use this command.");
+			return;
+		}
+		
+		if(args != null) {
+			player.sendMessage(Constants.CITYSCAPE + Constants.ERROR_COLOR +
+					"That command does not take arguments.");
+			return;
+		}
+		
+		ArrayList<String> cities = plugin.getDB().getInvites(player.getName());
+		
+		if(cities.size() == 0) {
+			player.sendMessage(Constants.CITYSCAPE + Constants.ERROR_COLOR +
+					"You have not been invited to join any cities.");
+			return;
+		}
+		
+		player.sendMessage(Constants.CITYSCAPE + Constants.SUCCESS_COLOR +
+				"You have been invited to join the cities of: ");
+		for(String city : cities)
+			player.sendMessage(Constants.SUCCESS_COLOR + city);
+		player.sendMessage(Constants.CITYSCAPE + Constants.SUCCESS_COLOR +
+				"Type /c accept cityname to accept an invitation.");
+		player.sendMessage(Constants.CITYSCAPE + Constants.SUCCESS_COLOR +
+				"Type /c decline cityname to decline an invitiation.");
 	}
 
 }
